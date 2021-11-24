5b8742d53a288315b21f6162fc42c9029ef2e5ce
KSG <ksg05340@gmail.com>
2021-11-24 23:40:57 +0900

    draw.h inoutro.h&cc -> cc not completed macros.h

---
+++ draw.h
@@ -0,0 +1,8 @@
+#ifndef _draw_h_
+#define _draw_h_
+
+// for draw, we need these two header file
+#include <ncurses.h>
+#include <unistd.h>
+
+#endif
---
+++ macros.h
@@ -0,0 +1,13 @@
+#ifndef _macros_h_
+#define _macors_h_
+
+#define MAP_WIDTH 20
+#define MAP_HEIGHT 20
+#define INTRO_HEIGHT 30
+#define OUTRO_HEIGHT 30
+
+
+#define PAUSE 1
+
+#endif
+
